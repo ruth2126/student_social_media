7d2e7cfba1e1711f53fc84cfbe464762f9f346d9
ruth2126 <ruthoyinlola@yahoo.co.uk>
2025-06-05 20:40:20 +0100

    preprocessing started

--- student_app.py
+++ student_app.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import streamlit as st
+
+df = pd.read_csv("C:/Users/Ruthf/Documents/student social media/Students Social Media Addiction.csv")
+print(df.head())
+print(df.shape)
+print(df.columns)
+print(df.describe())
+print(df.dtypes)
